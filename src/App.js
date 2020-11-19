43f2dba4d8b6cd24c1049adaf70eecff0dab7220
lucas <lucasf854@gmail.com>
2020-11-19 16:45:13 -0300

    equalization

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React,{Component} from 'react';
 import './App.css';
 import Cropper from './components/Cropper'; 
 import image from './1119.jpg'
+import unequalized from './Unequalized_Hawkes_Bay_NZ.jpg'
+  
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
             value: "",
-            img: image,
+            img: unequalized,
             cropDisabled: true
         }
         this.srcRef = React.createRef()
         this.desRef = React.createRef()
+        this.orRef = React.createRef()
     }
 
     handleChange = (event) =>{
@@ -30,14 +33,17 @@ class App extends Component {
 
     imgLoad = ()=>{
         const srcRef = this.srcRef.current
+        const orRef = this.orRef.current
         let desRef = this.desRef.current
         /*global cv*/
         let mat = cv.imread(this.srcRef.current)
         let dst = new cv.Mat();
+        // let dst = []
         cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY, 0);
-        // cv.equalizeHist(mat.data,dst)
+        cv.equalizeHist(mat,dst)
         // cv.imwrite('res.png')
-        cv.imshow(desRef, mat);
+        cv.imshow(orRef, mat);
+        cv.imshow(desRef, dst);
         console.log(dst)
         console.log(mat)
         // console.log(equ)
@@ -52,6 +58,7 @@ class App extends Component {
                 </div>
                 <Cropper src={img} disabled={cropDisabled}/>
                 <img id='src' ref={this.srcRef} src={this.state.img} onLoad={this.imgLoad} />
+                <canvas id='dst' ref={this.orRef} />
                 <canvas id='dst' ref={this.desRef} />
                 <button onClick={this.handleCrop}>Cortar</button>
             </div>)

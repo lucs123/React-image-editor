cef9572fdcf9670c401bdece06f98b1b3566c24e
lucas <lucasf854@gmail.com>
2020-11-20 17:50:01 -0300

    change db(not working)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import image from './1119.jpg'
 import unequalized from './Unequalized_Hawkes_Bay_NZ.jpg'
 import Editor from './components/Editor'
 import ImgList from './components/ImgList'
+import DbList from './components/DbList'
   
 
 class App extends Component {
@@ -12,7 +13,14 @@ class App extends Component {
         this.state = {
             img: unequalized,
             images: [],
-            databases: []
+            databases: [
+                {
+                    name: 'Banco 1',
+                    images: []
+                }
+            ],
+            banco: 'Banco 1',
+            nBanco: 1
         }
         this.editorRef = React.createRef()
     }
@@ -36,6 +44,58 @@ class App extends Component {
         this.editorRef.current.handleChange()
     }
 
+    // componentDidMount(){
+    //     this.createDB()
+    // }
+
+    createDB = ()=>{
+        const nBanco = this.state.nBanco + 1
+        const name = 'Banco ' + nBanco
+        const newDb = {
+            name: name,
+            images: []
+        }
+        this.setState(state=>{
+            const databases = state.databases.concat(newDb)
+            return{
+                databases: databases,
+                nBanco: nBanco
+            }
+        })
+    }
+
+    changeDb = (event)=>{
+        console.log(event.target.text)
+        const newDb = event.target.text 
+        const currentBanco = this.state.banco
+        if(currentBanco !== newDb){
+        const currentImages = this.state.images
+        const currentImage = this.editorRef.current.getImage()
+        currentImages.push(currentImage)
+        let newImages = []
+        this.setState(state=>{
+            const databases = state.databases.map(database=>{
+                // console.log(database)
+                if(database.name === currentBanco){
+                    database.images = currentImages
+                }
+                if(database.name === newDb){
+                    newImages = database.images
+                }
+            })
+            return{
+                banco: newDb,
+                images: newImages,
+                img: newImages[0],
+                databases: databases
+            }
+        })
+    }}
+
+    log = ()=>{
+        console.log(this.state.databases)
+    }
+
     changeImage = async (event)=>{
         const image = event.target.src
         const currentImage = this.editorRef.current.getImage()
@@ -57,7 +117,9 @@ class App extends Component {
                     <input type="file" accept="image/*" onChange={this.handleUpload} />
                 </div>
                 <Editor ref={this.editorRef} img={this.state.img}/>
+                <DbList databases={this.state.databases} createDB={this.createDB} changeDb={this.changeDb}/>
                 <ImgList images={this.state.images} changeImage={this.changeImage}/>
+                <button onClick={this.log}>Log</button>
             </div>)
     };
 }

e92655532a8d10bc967552a9df1e1c099413784f
lucas <lucasf854@gmail.com>
2020-11-20 13:40:56 -0300

    image list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import image from './1119.jpg'
 import unequalized from './Unequalized_Hawkes_Bay_NZ.jpg'
 import Editor from './components/Editor'
+import ImgList from './components/ImgList'
   
 
 class App extends Component {
@@ -16,10 +17,25 @@ class App extends Component {
         this.editorRef = React.createRef()
     }
 
-    handleUpload = async (event) =>{
+    setImage = async (event) =>{
         const newImage = URL.createObjectURL(event.target.files[0])
         await this.setState({img: newImage})
         this.editorRef.current.handleChange()
+    }
+
+    handleUpload = (event) =>{
+        // const uploadedFile = event.target.files[]0
+        // console.log(uploadedFile)
+        // let newImages = []
+        // uploadedFiles.map(file=>{
+        //     newImages.push(URL.createObjectURL(file))
+        // })
+        const newImage = URL.createObjectURL(event.target.files[0])
+        this.setState(state=>{
+            const images = state.images.concat(newImage)
+            return{images}
+        })
+        console.log(this.state.images)
     }
 
     render () { 
@@ -29,6 +45,7 @@ class App extends Component {
                     <input type="file" accept="image/*" onChange={this.handleUpload} />
                 </div>
                 <Editor ref={this.editorRef} img={this.state.img}/>
+                <ImgList images={this.state.images}/>
             </div>)
     };
 }

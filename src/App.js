a91b3b41c3c9e5b0bb1d5ed706c6d8497515c8fe
lucas <lucasf854@gmail.com>
2020-11-20 13:16:55 -0300

    pass image to child

--- src/App.js
+++ src/App.js
@@ -1,175 +1,34 @@
 import React,{Component} from 'react';
 import './App.css';
-import Cropper from 'cropperjs'; 
 import image from './1119.jpg'
 import unequalized from './Unequalized_Hawkes_Bay_NZ.jpg'
-import "cropperjs/dist/cropper.min.css";
+import Editor from './components/Editor'
   
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
-            value: "",
             img: unequalized,
-            equalized: false,
-            oldImages: [],
-            imageDestination: null
+            images: [],
+            databases: []
         }
-        this.srcRef = React.createRef()
-        this.cnvRef = React.createRef()
-        this.desRef = React.createRef()
-        this.orRef = React.createRef()
-        this.cropper = null
+        this.editorRef = React.createRef()
     }
 
-    handleChange = async (event) =>{
-        this.clearCanvas()
+    handleUpload = async (event) =>{
         const newImage = URL.createObjectURL(event.target.files[0])
-        this.srcRef.current.src = newImage
-    }
-
-    clearCanvas = ()=>{
-        this.cropper.destroy()
-        const canvas = this.cnvRef.current
-        console.log(canvas)
-        const context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.width, canvas.height);
-    }
-
-    handleDownload = ()=>{
-        const canvas = this.cnvRef.current
-        const link = document.createElement('a');
-        link.download = 'new_image.png';
-        link.href = canvas.toDataURL()
-        link.click();
-    }
-
-    backupImage = ()=>{
-        const canvas = this.cnvRef.current
-        const image = canvas.toDataURL("image/png")
-        const equalized = this.state.equalized
-        const oldImage = {
-            image,
-            equalized
-        }
-        this.setState(state=>{
-            const oldImages = state.oldImages.concat(oldImage)
-            return{oldImages};
-        })
-    }
-
-    undoChange = ()=>{
-        const oldImages = this.state.oldImages 
-        const image = oldImages[oldImages.length - 1].image 
-        const equalized = oldImages[oldImages.length - 1].equalized 
-        this.clearCanvas()
-        this.srcRef.current.src = image
-        oldImages.splice(oldImages.length-1,1) 
-        this.setState({
-            oldImages:oldImages,
-            equalized:equalized
-        })
-    }
-
-    componentDidMount(){
-        this.srcRef.current.src = unequalized
-    }
-
-    imgLoad = ()=>{
-        this.drawImage()
-    }
-
-    drawImage = ()=>{
-        const image = this.srcRef.current
-        const canvas = this.cnvRef.current
-        canvas.width = image.width
-        canvas.height = image.height
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(image,0,0,image.width,image.height);
-        this.enableCrop()
-    }
-
-    enableCrop = () =>{
-        const canvas = this.cnvRef.current
-        const cropper = new Cropper(canvas, {
-            zoomable: true,
-        // scalable: false,
-        // aspectRatio: 1,
-            autoCrop: false,
-            background: false,
-        crop(event) {
-
-        },
-        });
-        this.cropper = cropper
-    }
-
-    getCropped = ()=>{
-        this.backupImage()
-        const cropped = this.cropper.getCroppedCanvas()
-        const newImage = cropped.toDataURL("image/png")
-        this.clearCanvas()
-        this.srcRef.current.src = newImage
-    }
-
-    handleRotate = ()=>{
-        this.cropper.rotate(90)
-    }
-
-    handleCrop = ()=>{
-        this.cropper.setDragMode('crop')
-    }
-
-    handleMove = ()=>{
-        this.cropper.setDragMode('move')
-    }
-
-    handleZoom = ()=>{
-        console.log(this.cropper)
-        this.cropper.zoomable = true
-        this.cropper.zoomOnWheel = true
-    }
-
-    equalize = ()=>{
-            if(!this.state.equalized){
-                this.backupImage()
-                this.cropper.destroy()
-                /*global cv*/
-                const image = this.srcRef.current
-                const canvas = this.cnvRef.current
-                let mat = cv.imread(image)
-                let dst = new cv.Mat();
-                cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY, 0);
-                cv.equalizeHist(mat,dst)
-                cv.imshow(canvas, dst);
-                this.enableCrop()
-                this.setState({equalized: true})
-        }
+        await this.setState({img: newImage})
+        this.editorRef.current.handleChange()
     }
 
     render () { 
         return(  
             <div className="App">
                 <div>
-                    <input type="file" accept="image/*" onChange={this.handleChange} />
+                    <input type="file" accept="image/*" onChange={this.handleUpload} />
                 </div>
-                <div className={"img-container"}>
-                    <canvas id='src' ref={this.cnvRef}  />
-                </div>
-                <div>
-                    <img ref={this.srcRef} width='50%' style={{display:'none'}} onLoad={this.imgLoad}/>
-                </div>
-                <button onClick={this.handleCrop}>Modo Cortar</button>
-                <button onClick={this.handleMove}>Move</button>
-                <button onClick={this.equalize}>Equalizar</button>
-                <button onClick={this.handleDownload}>Download</button>
-                <button onClick={this.handleRotate}>Left</button>
-                <button onClick={this.handleRotate}>Right</button>
-                <button onClick={this.handleZoom}>Enable Zoom</button>
-                <button onClick={this.clearCanvas}>Limpar</button>
-                <button onClick={this.getCropped}>Cortar</button>
-                {this.state.oldImages.length ? <button onClick={this.undoChange}>Undo</button> : null }
+                <Editor ref={this.editorRef} img={this.state.img}/>
             </div>)
     };
 }
